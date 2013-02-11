bd34e39fa32ab7ac064cc1eba5a30ff4322c5b47
VGlushchenko <heavyck@gmail.com>
2013-02-11 12:48:30 +0200

    Transaction Controller

--- src/main/java/com/in6k/mypal/controller/TransactionController.java
+++ src/main/java/com/in6k/mypal/controller/TransactionController.java
@@ -1,4 +1,26 @@
 package com.in6k.mypal.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class TransactionController {
+
+    @RequestMapping(value = "/createtransaction")
+    public String create(@RequestParam("userId") String id) {
+
+        return "create";
+    }
+
+    @RequestMapping(value = "/transactions")
+    public String list() {
+        return "list";
+    }
+
+    @RequestMapping(value = "/deletetransactions")
+    public String delete() {
+        return "list";
+    }
+
 }
