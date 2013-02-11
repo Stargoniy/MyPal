b3d788a3801b8e6be70137d75db6a928f8f71d3e
VGlushchenko <heavyck@gmail.com>
2013-02-11 14:05:59 +0200

    transactionController

--- src/main/java/com/in6k/mypal/controller/TransactionController.java
+++ src/main/java/com/in6k/mypal/controller/TransactionController.java
@@ -3,6 +3,7 @@ package com.in6k.mypal.controller;
 import com.in6k.mypal.dao.TransactionDAO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.SQLException;
@@ -11,8 +12,43 @@ import java.sql.SQLException;
 @RequestMapping(value = "/transaction")
 public class TransactionController {
 
-    @RequestMapping(value = "/create")
+    @RequestMapping(value = "/create", method = RequestMethod.GET )
+    public String creationForm(@RequestParam("debit") String debit, @RequestParam("credit") String credit) {
+        /*Integer debitId = new Integer(debit);
+        Integer creditId = new Integer(credit);
+
+        User user = null;
+
+            user = UserDAO.getById(debitId);
+
+
+        Account acc = new Account();
+        acc.setName(request.getParameter("name"));
+        acc.setUser(user);
+        acc.setTypeId(25);
+
+        AccountDAOimpl.save(acc);*/
+
+
+        return "create";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST )
     public String create(@RequestParam("debit") String debit, @RequestParam("credit") String credit) {
+        /*Integer debitId = new Integer(debit);
+        Integer creditId = new Integer(credit);
+
+        User user = null;
+
+            user = UserDAO.getById(debitId);
+
+
+        Account acc = new Account();
+        acc.setName(request.getParameter("name"));
+        acc.setUser(user);
+        acc.setTypeId(25);
+
+        AccountDAOimpl.save(acc);*/
 
 
         return "create";
