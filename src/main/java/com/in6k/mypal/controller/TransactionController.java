b99c124fe5426f2e28c361056533663c260a419c
VGlushchenko <heavyck@gmail.com>
2013-02-12 09:52:48 +0200

    commit

--- src/main/java/com/in6k/mypal/controller/TransactionController.java
+++ src/main/java/com/in6k/mypal/controller/TransactionController.java
@@ -4,6 +4,7 @@ import com.in6k.mypal.dao.TransactionDAO;
 import com.in6k.mypal.dao.UserDao;
 import com.in6k.mypal.domain.Transaction;
 import com.in6k.mypal.domain.User;
+import com.in6k.mypal.services.TransactionValidator;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,14 +32,17 @@ public class TransactionController {
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public String create(HttpServletRequest request) throws IOException {
         TransactionValidator transactionValidator = new TransactionValidator();
+
         transactionValidator.setCredit(UserDao.getById(Integer.parseInt(request.getParameter("credit"))));
         transactionValidator.setDebit(UserDao.getByEmail(request.getParameter("debit")));
         transactionValidator.setSum(Double.parseDouble(request.getParameter("sum")));
+
         if (transactionValidator.validate().size() == 0) {
             Transaction transaction = new Transaction();
             transaction.setDebit(transactionValidator.getDebit());
             transaction.setCredit(transactionValidator.getCredit());
             transaction.setSum(transactionValidator.getSum());
+
             TransactionDAO.create(transaction);
 
             return "transaction/list";
@@ -54,10 +58,7 @@ public class TransactionController {
 
     @RequestMapping(value = "/list")
     public String list(ModelMap model) throws IOException, SQLException {
-
-        Collection<Transaction> transactions = null;
-
-        transactions = TransactionDAO.findAllForUser(UserDao.getById(2));
+        Collection<Transaction> transactions = TransactionDAO.list();;
 
         model.addAttribute("transactions", transactions);
 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <html>
+<head><link type="text/css" rel="stylesheet" href="/css/bootstrap.css" /></head>
 <body>
 <h2>Hello World!</h2>
+<button type="submit" class="btn btn-primary">Create</button>
 </body>
 </html>
