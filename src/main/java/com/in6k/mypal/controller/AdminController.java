62ac1b4c8cf36d68964080dcb8adcc1e6e00e11e
inflammable400 <rezzident@mail.ru>
2013-02-14 16:41:56 +0200

    added trasaction canceling

--- src/main/java/com/in6k/mypal/controller/AdminController.java
+++ src/main/java/com/in6k/mypal/controller/AdminController.java
@@ -19,7 +19,7 @@ import java.sql.SQLException;
 @Controller
 public class AdminController {
 
-    private boolean isAdminLogged;
+    private boolean isAdminLogged = false;
 
     @Autowired
     private AdminService adminService;
@@ -46,50 +46,80 @@ public class AdminController {
     public String showRegistredUsers(ModelMap model) {
 
         if (isAdminLogged) {
-        model.addAttribute("userlist", UserDao.list());
+            model.addAttribute("userlist", UserDao.list());
 
-        return "/user/list";
+            return "/user/list";
         }
+
         return "/security/login";
     }
 
     @RequestMapping(value = "/user/ban/{id}", method = RequestMethod.GET)
-    public void BanUser ( @PathVariable int id, HttpServletResponse response) throws IOException {
+    public String BanUser ( @PathVariable int id, HttpServletResponse response) throws IOException {
+        if (isAdminLogged) {
+            adminService.banUser(id);
 
-        adminService.banUser(id);
+            response.sendRedirect("/users");
+        }
 
-        response.sendRedirect("/users");
+        return "/security/login";
     }
 
     @RequestMapping(value = "/user/unban/{id}", method = RequestMethod.GET)
-    public void UnBanUser ( @PathVariable int id, HttpServletResponse response) throws IOException {
+    public String UnBanUser ( @PathVariable int id, HttpServletResponse response) throws IOException {
+        if (isAdminLogged) {
+            adminService.unBanUser(id);
 
-        adminService.unBanUser(id);
+            response.sendRedirect("/users");
+        }
 
-        response.sendRedirect("/users");
+        return "/security/login";
     }
 
     @RequestMapping(value = "users/{id}/transactions", method = RequestMethod.GET)
     public String showTransactionsOfUser(ModelMap model, @PathVariable int id) {
+        if (isAdminLogged) {
+            model.addAttribute("transactions", adminService.transactionsForUserById(id));
 
-        model.addAttribute("transactions", adminService.transactionsForUserById(id));
+            return "admin/transactionlist";
+        }
 
-        return "admin/transactionlist";
+        return "/security/login";
     }
 
     @RequestMapping(value = "/admin/transaction/list")
     public String showAllTransactions(ModelMap model) throws IOException, SQLException {
 
-        model.addAttribute("transactions", adminService.showAllTransactions());
+        if (isAdminLogged) {
+            model.addAttribute("transactions", adminService.showAllTransactions());
 
-        return "/admin/transactionlist";
+            return "/admin/transactionlist";
+        }
+
+        return "/security/login";
     }
 
-    @RequestMapping(value = "transaction/cancel", method = RequestMethod.GET)
-    public String cancelTransaction(@PathVariable int id) throws SQLException {
+    @RequestMapping(value = "/transaction/{id}/cancel", method = RequestMethod.GET)
+    public String cancelTransaction(@PathVariable int id, HttpServletResponse response) throws SQLException, IOException {
 
-        adminService.cancelTransaction(id);
+        if (isAdminLogged) {
+            adminService.cancelTransaction(id);
 
-        return "admin/transactionlist";
+            response.sendRedirect("/admin/transaction/list");
+        }
+
+        return "/security/login";
+    }
+
+    @RequestMapping(value = "/transaction/{id}/restore", method = RequestMethod.GET)
+    public String restoreTransaction(@PathVariable int id, HttpServletResponse response) throws SQLException, IOException {
+
+        if (isAdminLogged) {
+            adminService.restoreTransaction(id);
+
+            response.sendRedirect("/admin/transaction/list");
+        }
+
+        return "/security/login";
     }
 }
